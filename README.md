15dd63edb6ad9cbd4ce325dc86263046932e872d
ahwelgemoed <arno.welgemoed@outlook.com>
2022-06-21 15:21:45 +0200

    feat: :monocle_face: init me please

---
+++ README.md
@@ -0,0 +1,9 @@
+## A simple TS script to get Mendix Marketplace Data
+
+Add Widget URLs to `src/marketplaceItems.ts`
+
+Run `npm run build`
+
+Run `npm run check`
+
+This wil generate a folder in the data folder with all the screenshots of the marketplace in it with the "data' in `widgets.csv`
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { createWriteStream, mkdirSync, existsSync } from "node:fs";
+import { chromium } from "playwright";
+import * as csv from "fast-csv";
+import { marketplaceItems } from "./marketplaceItems";
+import { today } from "./utils";
+
+const output = __dirname + `/../data/${today()}`;
+
+const csvStream = csv.format({ headers: true });
+
+if (!existsSync(output)) {
+  mkdirSync(output, { recursive: true });
+}
+
+const writeStream = createWriteStream(`${output}/widgets.csv`);
+
+(async () => {
+  const browser = await chromium.launch({ headless: true });
+  for (const mpItem of marketplaceItems) {
+    console.log("Marketplace URL 💻", mpItem);
+    const page = await browser.newPage();
+    await page.goto(mpItem);
+    const versionNumber = await page
+      .locator("p", { hasText: "Version" })
+      .first()
+      .innerText();
+    const publicationDate = await page
+      .locator("p", { hasText: "Date:" })
+      .first()
+      .innerText();
+    const downloads = await page
+      .locator(".pds-u-icon-download-bottom-before")
+      .first()
+      .innerText();
+    const name = await page.locator("h1").first().innerText();
+
+    const vote = await page.locator(".mx-name-container_reviewContent");
+    let lastVote = "";
+
+    if (vote.count()) {
+      lastVote = await vote.first().innerText();
+    }
+
+    await page.screenshot({ path: `${output}/${name}-${today()}.png` });
+
+    const widget = {
+      name,
+      versionNumber,
+      publicationDate,
+      downloads,
+      lastVote,
+    };
+    csvStream.write(widget);
+    await page.close();
+  }
+
+  await browser.close();
+  csvStream.pipe(writeStream);
+  csvStream.end();
+})();
