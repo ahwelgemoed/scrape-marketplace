376356555aa34bdadbabe800c051c8af789e3eb1
ahwelgemoed <arno.welgemoed@outlook.com>
2022-06-22 11:48:42 +0200

    feat: :fire: make read text file and `xslx` output

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-## A simple TS script to get Mendix Marketplace Data
-
-Add Widget URLs to `src/marketplaceItems.ts`
+## A simple TS script to get Mendix Marketplace Data (Public Only)
 
 Run `npm run build`
 
+Create A Text file called `links.txt` with Marketplace URLS (Enter Separated)
+
 Run `npm run check`
 
-This wil generate a folder in the data folder with all the screenshots of the marketplace in it with the "data' in `widgets.csv`
+This wil generate a folder in the data folder with all the screenshots of the marketplace in it with the "data' in `widgets.xlsx` and `widgets.json`
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,65 @@
-import { createWriteStream, mkdirSync, existsSync } from "node:fs";
+import { readFile, writeFile } from "node:fs/promises";
+import { mkdirSync, existsSync } from "node:fs";
 import { chromium } from "playwright";
-import * as csv from "fast-csv";
-import { marketplaceItems } from "./marketplaceItems";
+import * as xl from "excel4node";
 import { today } from "./utils";
 
-const output = __dirname + `/../data/${today()}`;
+const wb = new xl.Workbook();
 
-const csvStream = csv.format({ headers: true });
+const ws = wb.addWorksheet("WIDGETS");
+
+const style = wb.createStyle({
+  font: {
+    color: "black",
+    size: 12,
+  },
+  numberFormat: "$#,##0.00; ($#,##0.00); -",
+});
+
+const jsonArray = [];
+
+const output = __dirname + `/../data/${today()}`;
+const excelPath = `${output}/widgets.xlsx`;
+const jsonPath = `${output}/widgets.json`;
 
 if (!existsSync(output)) {
   mkdirSync(output, { recursive: true });
 }
 
-const writeStream = createWriteStream(`${output}/widgets.csv`);
-
+const titles = [
+  "name",
+  "description",
+  "versionNumber",
+  "publicationDate",
+  "downloads",
+  "lastVote",
+  "numberOfRatings",
+  "mxVersion",
+  "link",
+];
 (async () => {
+  const linksToVisit = await readFile(`${__dirname}/../links.txt`, "utf8");
+  const parsedLinks = linksToVisit.split("\n");
+  let count = 1;
+  for (const [i, title] of titles.entries()) {
+    ws.cell(count, i + 1)
+      .string(title)
+      .style(style);
+  }
+
   const browser = await chromium.launch({ headless: true });
-  for (const mpItem of marketplaceItems) {
+
+  for (const mpItem of parsedLinks) {
     console.log("Marketplace URL 💻", mpItem);
+    count++;
     const page = await browser.newPage();
-    await page.goto(mpItem);
-    const versionNumber = await page
+    await page.goto(mpItem.trim());
+    const sideBar = await page.locator(".pds-u-max-width-256");
+    const versionNumber = await sideBar
       .locator("p", { hasText: "Version" })
       .first()
       .innerText();
-    const publicationDate = await page
+    const publicationDate = await sideBar
       .locator("p", { hasText: "Date:" })
       .first()
       .innerText();
@@ -32,29 +67,51 @@ const writeStream = createWriteStream(`${output}/widgets.csv`);
       .locator(".pds-u-icon-download-bottom-before")
       .first()
       .innerText();
+    const numberOfRatings = await page
+      .locator(".col-lg-auto:has(p.pds-c-text-14)")
+      .first()
+      .innerText();
+    const mxVersion = await sideBar
+      .locator("p", { hasText: "Requires" })
+      .first()
+      .innerText();
+
     const name = await page.locator("h1").first().innerText();
 
     const vote = await page.locator(".mx-name-container_reviewContent");
+    const description = await page
+      .locator(".mx-name-cKEditorViewerForMendix1")
+      .first()
+      .innerText();
+
     let lastVote = "";
 
     if (await vote.count()) {
       lastVote = await vote.first().innerText();
     }
-
     await page.screenshot({ path: `${output}/${name}-${today()}.png` });
-
     const widget = {
       name,
-      versionNumber,
-      publicationDate,
-      downloads,
-      lastVote,
+      link: mpItem,
+      versionNumber: versionNumber ?? "",
+      publicationDate: publicationDate ?? "",
+      downloads: downloads ?? "",
+      lastVote: lastVote ?? "",
+      numberOfRatings: numberOfRatings ?? "",
+      mxVersion: mxVersion ?? "",
+      description: description ?? "",
     };
-    csvStream.write(widget);
+    jsonArray.push(widget);
+
+    for (const [i, title] of titles.entries()) {
+      ws.cell(count, i + 1)
+        .string(widget[title])
+        .style(style);
+    }
     await page.close();
   }
 
+  await writeFile(jsonPath, JSON.stringify(jsonArray));
+  await wb.write(excelPath);
   await browser.close();
-  csvStream.pipe(writeStream);
-  csvStream.end();
 })();
